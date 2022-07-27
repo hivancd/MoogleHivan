67d66c46c77173848e6dea39849d5602a4075dee
HivanElTerrible <hivancdiaz@gmail.com>
2022-07-27 10:16:19 -0500

    commit inicial

---
+++ MoogleEngine/Moogle.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+
+namespace MoogleEngine;
+
+public static class Moogle
+{
+    // TODO: Encontrar Frecuencia en el corpus
+    // TODO: Encontrar Repeticiones en el archivo
+    // Calcular TFIDF y comparar, coger 3 mas altos
+    // Hacer Sugerncias
+
+    public static SearchItem[] DescendingSort(SearchItem[] array)//Solo funciona con arrays de  numeros positivos
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i + 1; j<array.Length;j++)
+            {
+                if (array[j].Score>array[i].Score)
+                {
+                    SearchItem max = array[j];
+                    array[j] = array[i];
+                    array[i] = max;
+                }
+            }
+        }
+        return array;
+    }
+    public static SearchItem Max(SearchItem[] array)
+    {
+        SearchItem Max = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Score > Max.Score)
+                Max = array[i];
+        }
+        return Max;
+    }
+
+    public static int CountOcurrences(string text, string subs)
+    {
+        if (subs == "")
+            return 0;//No pincha porque el substring es ""
+        int occurrences = 0;
+        int start = text.IndexOf(subs);
+
+        if (start != -1)
+        {
+            occurrences++;
+            occurrences = occurrences + CountOcurrences(text.Substring(start + subs.Length), subs);
+        }
+        return occurrences;
+    }
+    public static SearchResult Query(string query)
+    {
+        // Modifique este método para responder a la búsqueda
+
+        string content = @"E:\Prog\00moogle\moogle-main\Content";
+        string[] files = Directory.GetFiles(content);
+        SearchItem[] FilesOccur = new SearchItem[files.Length];
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            string CurrentFile = File.ReadAllText(files[i]);
+            string FileName = Path.GetFileNameWithoutExtension(files[i]);
+            string snippet = "Fill the snippet dont be lazy";
+            FilesOccur[i] = new SearchItem(FileName, snippet, CountOcurrences(CurrentFile, query));
+        }
+
+        FilesOccur = DescendingSort(FilesOccur);
+
+        SearchItem[] items =
+        {
+            FilesOccur[0],
+            FilesOccur[1],
+            FilesOccur[2]
+
+            // new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
+            // new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
+            // new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
+        };
+        return new SearchResult(items, query);
+    }
+}
---
+++ Testin/Program.cs
@@ -0,0 +1,86 @@
+﻿using MoogleEngine;
+
+bool CheckSort()
+{
+    SearchItem[] Example =
+    {
+    new SearchItem("Ultimo", "Lorem ipsum dolor sit amet", 0),
+    new SearchItem("Primero", "Lorem ipsum dolor sit amet", 9),
+    new SearchItem("Segundo", "Lorem ipsum dolor sit amet", 8),
+    };
+
+
+    var ans = Moogle.DescendingSort(Example);
+    foreach (SearchItem SIU in ans)
+        System.Console.WriteLine(SIU.Title);
+    if (ans[0].Title != "Primero")
+        return false;
+
+    SearchItem[] Example1 =
+       {
+    new SearchItem("Ultimo", "Lorem ipsum dolor sit amet", 9),
+    new SearchItem("Primero", "Lorem ipsum dolor sit amet", 9),
+    new SearchItem("Segundo", "Lorem ipsum dolor sit amet", 8),
+    };
+
+
+    ans = Moogle.DescendingSort(Example1);
+    foreach (SearchItem SIU in ans)
+        System.Console.WriteLine(SIU.Title);
+    if (ans[0].Score != 9)
+        return false;
+
+    return true;
+}
+
+if (!CheckSort())
+{
+    System.Console.WriteLine("EL ORDENAMIENTO DE LOS ARRAY ESTA ROTO");
+}
+
+
+
+
+
+// System.Console.WriteLine("JDFKF");
+// int[] DescendingSort(int[] array)//Solo funciona con arrays de  numeros positivos
+// {
+//     int[] newArray = new int[array.Length];
+
+//     for(int i = 0; i<array.Length; i++)
+//     {
+//         int a = Max(array);
+//         int Index = Array.IndexOf(array, a);
+//         array[Index] = -1;
+//         newArray[i] = a;
+//     }
+//     return newArray;
+// }
+// int Max(int[] array)
+// {
+//     int Max = array[0];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > Max)
+//             Max = array[i];
+//     }
+//     return Max;
+// }
+
+
+// foreach(int i in DescendingSort(array))
+// {
+//     System.Console.WriteLine(i);
+// }
+
+
+
+
+
+
+
+
+// System.Console.WriteLine("abcasdfwefabc".IndexOf("abcdsd"));
+
+
+
