82a0249e120443ce20a5c2353ce922e9ac7b0634
HivanElTerrible <hivancdiaz@gmail.com>
2022-08-01 09:02:18 -0500

    Ordenando

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -4,7 +4,7 @@ namespace MoogleEngine;
 
 public static class Moogle
 {
-        // public static SearchItem Max(SearchItem[] array)//Este Metodo No es Necesario
+    // public static SearchItem Max(SearchItem[] array)//Este Metodo No es Necesario
     // {
     //     SearchItem Max = array[0];
     //     for (int i = 0; i < array.Length; i++)
@@ -19,24 +19,58 @@ public static class Moogle
     //No encuentra palabras acompannadas  de caracteres especiales(:,#,.,etc...)
     // Hacer Sugerncias
 
-    public static SearchItem[] DescendingSort(SearchItem[] array)//Ordena Un Array De SearchItems de mayor a menor segun el Score
-    {
-        for (int i = 0; i < array.Length; i++)
-        {
-            for (int j = i + 1; j < array.Length; j++)
-            {
-                if (array[j].Score > array[i].Score)
-                {
-                    SearchItem max = array[j];
-                    array[j] = array[i];
-                    array[i] = max;
-                }
-            }
-        }
-        return array;
-    }
 
-    public static int CountOcurrences(string text, string subs)//Cuenta las ocurrencias de una palabra en un texto
+
+    public static string GetSnip(string text, string word)//Este metodo coge el snip (Es mejorable)ARREGLAR
+    {
+        int middle = text.IndexOf(word);
+        int SnipLeng = 30;
+
+        if (middle > 0)
+        {
+            if (text.Length - middle <= SnipLeng)
+                return text.Substring(middle, text.Length);
+            if (middle < 15)
+                return text.Substring(0, SnipLeng);
+            else
+                return text.Substring(middle - 15, SnipLeng);
+        }
+        return " ";
+    }
+    public static string erase_stopwords(string sentence)//This method erases stopwords from the queryQUERY PROCESSING
+    {
+        string[] array_sentence = sentence.Split();
+        string ans = "";
+
+        for (int i = 0; i < array_sentence.Length; i++)
+        {
+            if (!Is_stop_word(array_sentence[i]))
+                ans = ans + array_sentence[i] + " ";
+        }
+        return ans;
+    }
+    public static string erase_whitespace(string sentence)//this method erases whitespace from the queryQUERY PROCESSING
+    {
+        string ans = "";
+
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            if (!(sentence[i].ToString() == " " && ((i + 1 == sentence.Length) || (sentence[i + 1].ToString() == " "))))
+                ans = ans + sentence[i];
+        }
+        return ans;
+    }
+    public static bool Is_stop_word(string word)//this method dtermines if a word is a stopwordQUERY PROCESSING
+    {
+        string stop_words_archive = @"E:\Prog\00moogle\moogle-main\stopwords.txt";
+        string[] stopwords = File.ReadAllLines(stop_words_archive);
+
+        if (stopwords.Contains(word.ToLower()))
+            return true;
+        return false;
+    }
+    //Cuenta las ocurrencias de una palabra en un texto
+    public static int CountOcurrences(string text, string subs)
     {
         if (subs == "")
             return 0;//No pincha porque el substring es ""
@@ -54,8 +88,20 @@ public static class Moogle
         return occurrences;
     }
 
-    public static int SentenceCountOc(string text, string sentence)//Cuenta las ocurrencias de cada palabra de 
-                                                                   // una oracion en un texto y las suma 
+    public static double tf(int raw_count, string text)
+    {
+        string[] text_array = text.Split();
+        return ((double)raw_count / (double)(text_array.Length));
+    }
+
+    public static double idf(int docs_in_corpus, int docs_with_occur)
+    {
+        if (docs_with_occur == 0 || docs_in_corpus == docs_with_occur)
+            return 0;
+        return Math.Log(10, (double)docs_in_corpus / (double)docs_with_occur);
+    }
+    //Cuenta las ocurrencias de cada palabra de una oracion en un texto y las suma
+    public static int SentenceCountOc(string text, string sentence)                                                               
     {
         string[] words = sentence.Split();
         int totalOccur = 0;
@@ -68,59 +114,23 @@ public static class Moogle
 
         return totalOccur;
     }
-
-    public static string GetSnip(string text, string word)//Este metod coge el snip (Es mejorable)
+    public static SearchItem[] DescendingSort(SearchItem[] array)//Ordena Un Array De SearchItems de mayor a menor segun el ScoreRESULTS PROCESSING
     {
-        int middle = text.IndexOf(word);
-        int SnipLeng = 30;
-
-        if (middle > 0)
+        for (int i = 0; i < array.Length; i++)
         {
-            if (text.Length - middle <= SnipLeng)
-                return text.Substring(middle, text.Length);
-            if (middle < 15)
-                return text.Substring(0, SnipLeng);
-            else
-                return text.Substring(middle - 15, SnipLeng);
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j].Score > array[i].Score)
+                {
+                    SearchItem max = array[j];
+                    array[j] = array[i];
+                    array[i] = max;
+                }
+            }
         }
-        return " ";
+        return array;
     }
-
-    public static string erase_stopwords(string sentence)//This method erases stopwords from the query
-    {
-        string[] array_sentence = sentence.Split();
-        string ans = "";
-
-        for (int i = 0; i < array_sentence.Length; i++)
-        {
-            if (!Is_stop_word(array_sentence[i]))
-                ans = ans + array_sentence[i] + " ";
-        }
-        return ans;
-    }
-
-    public static string erase_whitespace(string sentence)//this method erases whitespace from the query
-    {
-        string ans = "";
-
-        for (int i = 0; i < sentence.Length; i++)
-        {
-            if (!(sentence[i].ToString() == " " && ((i + 1 == sentence.Length) || (sentence[i + 1].ToString() == " "))))
-                ans = ans + sentence[i];
-        }
-        return ans;
-    }
-    public static bool Is_stop_word(string word)//this method dtermines if a word is a stopword
-    {
-        string stop_words_archive = @"E:\Prog\00moogle\moogle-main\stopwords.txt";
-        string[] stopwords = File.ReadAllLines(stop_words_archive);
-
-        if (stopwords.Contains(word.ToLower()))
-            return true;
-        return false;
-    }
-
-    public static SearchResult Query(string query)//Este es el metodo Principal
+    public static SearchResult Query(string query)//Este es el metodo PrincipalMAIN METHOD
     {
         // Modifique este método para responder a la búsqueda
 
