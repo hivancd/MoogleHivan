245f19bfcaaf902b8d4cc5a74bcaff47025bb5fa
HivanElTerrible <hivancdiaz@gmail.com>
2022-09-21 21:02:59 -0500

    MatricesyFuncionaBasic

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -12,141 +12,55 @@ public static class Moogle
         timer.Start();
 
         string content = @"E:\Prog\moogle\moogle-main\Content";
-        string[] files = Directory.GetFiles(content);
 
-        var Importance = GetImportance(query);
+
+        // var Importance = GetImportance(query);
         string search_query = QueryProcessing.ProcessQuery(query);
 
-        var Dictionarys = WordDictionarys.GetWordDictionarys(files);
-        Dictionary<string, int> query_and_docs_occur = Query_and_docs_occur(search_query, Dictionarys);
+        string[] files = Directory.GetFiles(content);
+        var Dictionarys = MatricesWork.GetWordDictionarys(files);
+        var AllTheWords = MatricesWork.GetAllTheWords(Dictionarys);
+        var QueryVec = MatricesWork.Sentence2Vec(search_query, AllTheWords);
+        var PreMatrix = MatricesWork.GetMatrix(files, Dictionarys, AllTheWords);
+        var Matrix = MatricesWork.tf_idf(PreMatrix);
+        var SearchValues = MatricesWork.VecXMatrix(QueryVec, Matrix);
+        SearchItem[] searchItems = GetSearchItems(SearchValues);
+        string Suggestion = "GetSuggestion";
+        SearchResult result = new SearchResult(searchItems,Suggestion);
+        
+        return result;
 
-        SearchItem[] FilesOccur = new SearchItem[files.Length];
 
-        for (int i = 0; i < files.Length; i++)
+        SearchItem[] GetSearchItems(double[] SearchValues)
         {
-            // Method: Construir el SearchItem
-            var text = Dictionarys[i];
-            string CurrentFile = File.ReadAllText(files[i]);
-            int FileSize = text.Values.Sum();
-            string FileName = Path.GetFileNameWithoutExtension(files[i]);
-            string snippet = GetSnip(CurrentFile, search_query);
+            List<SearchItem> items = new List<SearchItem>();
 
-            double Score = tf_idf(query_and_docs_occur, text,Importance, FileSize, files.Length, search_query) * (1 + 1 / GetCLoseWords(CurrentFile, query, text));
-
-            FilesOccur[i] = new SearchItem(FileName, snippet, (float)Score);
-            // tf(count Occur,CurrentFile)
-            // idf(files.Length,docsOccur)
+            // SearchItem(string title, string snippet, float score)
+            for (int i = 0; i < SearchValues.Length; i++)
+            {
+                string FileName = "";
+                string Snippet = "";
+                float Score = 0;
+                if (SearchValues[i] != 0)
+                {
+                    FileName = Path.GetFileNameWithoutExtension(files[i]);
+                    Snippet = GetSnip(files[i], search_query);
+                    Score = (float)SearchValues[i];
+                    SearchItem item = new(FileName, Snippet, Score);
+                    items.Add(item);
+                }
+            }
+            var itemArray = ResultsPreparations.SortSearchItems(items.ToArray());
+            return itemArray;
         }
 
-        var items = ResultsPreparations.SortSearchItems(FilesOccur);
-
-        // foreach (SearchItem item in items)
-        //     System.Console.WriteLine(item);
-
-        timer.Stop();
-        System.Console.WriteLine("Time taken: " + timer.Elapsed.ToString(@"m\:ss\.fff"));
-        timer.Restart();
-
-        timer.Start();
-        var suggestion = "Suggestion.GetSuggestion(files,search_query)";
-        // var suggestion = Suggestion.GetSuggestion(files,search_query);
-        timer.Stop();
-        System.Console.WriteLine("Time taken in suggestion: " + timer.Elapsed.ToString(@"m\:ss\.fff"));
-        return new SearchResult(items, suggestion);// Suggestion.GetSuggestion(files,search_query)
     }
-
-
-
-    public static double tf_idf(Dictionary<string, int> Query_docs_occur, Dictionary<string, int> text,Dictionary<string, int> Importance, int words_in_text, int docs_in_corpus, string query)
-    {
-        double tf_idf = 0;
-        var imp = 1;
-        List<string> NotAllowedWords = GetNotAllowedWords(query);
-        List<string> NecessaryWords = GetNecessaryWords(query);
-
-        foreach (string word in NecessaryWords)
-        {
-            if (!text.Keys.Contains(word))
-                return tf_idf;
-        }
-        foreach (string word in NotAllowedWords)
-        {
-            if (text.Keys.Contains(word))
-                return tf_idf;
-        }
-        foreach (string word in Query_docs_occur.Keys)
-        {
-            if(Importance.ContainsKey(word))
-                imp += Importance[word];
-            System.Console.WriteLine(word);
-            System.Console.WriteLine(Moogle.tf(word, text) * Moogle.idf(docs_in_corpus, Query_docs_occur[word])*imp);
-            // System.Console.WriteLine(imp);
-            tf_idf += Moogle.tf(word, text) * Moogle.idf(docs_in_corpus, Query_docs_occur[word])*imp;
-            imp = 1;
-        }
-
-        return tf_idf;
-    }
-    public static double tf(string subs, Dictionary<string, int> Dictionary)
-    {
-        int a = 0;
-        if (Dictionary.ContainsKey(subs))
-            a = Dictionary[subs];
-        return ((double)a / (double)Dictionary.Count);
-    }
-
-
-    public static double idf(int docs_in_corpus, int docs_with_occur)
-    {
-        if (docs_with_occur == 0 || docs_in_corpus == docs_with_occur)
-            return 0;
-        return Math.Log(10, (double)docs_in_corpus / (double)docs_with_occur);
-    }
-
-    public static List<string> GetNotAllowedWords(string query)
+    public static string GetSnip(string filePath, string query)//Este metodo coge el snip (Es mejorable)ARREGLAR
     {
         string[] query_array = query.Split();
-        List<string> GetNotAllowedWords = new List<string>();
-        if (query == "")
-            return GetNotAllowedWords;
-        foreach (string word in query_array)
-        {
-            if (word[0].ToString() == "!")
-                GetNotAllowedWords.Add(word.Substring(1));
-        }
-        return GetNotAllowedWords;
-    }
-    // static public int GetCLoseWords(string text, string query)
-    // {
-    //     string pattern = @"[ ~ ]||[~]||[ ~]||[~ ]";
-    //     Regex obj = new Regex(pattern);
-
-    //     List<int> distances = new List<int>();
-    //     int distance = 0;
-    //     int ans = 0;
-    //     string[] query_array = query.Split();
-
-    //     for (int i = 0; i < query_array.Length - 2; i++)
-    //     {
-    //         int LeftWordIndex = text.IndexOf(query_array[i]);
-    //         int RightWordIndex = text.IndexOf(query_array[i + 2]);
-
-    //         if (obj.IsMatch(query_array[i + 1]) && LeftWordIndex >= 0 && RightWordIndex >= 0)
-    //         {
-    //             distance = (Math.Abs(LeftWordIndex - RightWordIndex));
-    //             distances.Add(distance);
-    //         }
-    //     }
-    //     foreach (int e in distances)
-    //         ans += e;
-
-    //     return ans;
-    // }
-    public static string GetSnip(string text, string query)//Este metodo coge el snip (Es mejorable)ARREGLAR
-    {
-        string[] query_array = query.Split();
+        string text = File.ReadAllText(filePath);
         int middle = -1;
-        int SnipLeng = 30;
+        int SnipLeng = 300;
 
         foreach (string word in query_array)
         {
@@ -158,17 +72,18 @@ public static class Moogle
                 if (text.Length - middle <= SnipLeng)
                     return text.Substring(middle, text.Length - middle);
 
-                if (middle < 15)
+                if (middle < 150)
                     return text.Substring(0, SnipLeng);
                 else
-                    return text.Substring(middle - 15, SnipLeng);
+                    return text.Substring(middle - 150, SnipLeng);
             }
         }
         return " ";
     }
 
 
-    public static double GetCLoseWords(string text, string query, Dictionary<string, int> Dict)
+
+    static double GetCLoseWords(string text, string query, Dictionary<string, int> Dict)
     {
         string pattern = @"[ ~ ]||[~]||[ ~]||[~ ]";
         Regex obj = new Regex(pattern);
@@ -213,7 +128,7 @@ public static class Moogle
         return 1;
     }
 
-    public static int[] GetIndexes(string text, string word, int cant)
+    static int[] GetIndexes(string text, string word, int cant)
     {
         int[] AllIndexes = new int[cant];
         int wordIndex = text.IndexOf(word);
@@ -228,32 +143,7 @@ public static class Moogle
     }
 
 
-    // This method returns a dict with each word in the query and the amount of files in wich it appears
-    // public static Dictionary<string, int> Query_and_docs_occur(string search_query, string[] files)
-    // {
-    //     string[] search_query_array = search_query.Split();
-    //     Dictionary<string, int> dict = new Dictionary<string, int>(search_query_array.Length);
-
-    //     foreach (string word in search_query_array)
-    //         dict.Add(word, 0);
-
-    //     for (int i = 0; i < files.Length; i++)
-    //     {
-    //         string CurrentFile = File.ReadAllText(files[i]);
-
-    //         foreach (string word in search_query_array)
-    //         {
-    //             if (word == "" || word == " ")
-    //                 continue;
-    //             else if (CurrentFile.Contains(word))
-    //                 dict[word] += 1;
-    //         }
-    //     }
-    //     return dict;
-    // }
-
-
-    public static Dictionary<string, int> Query_and_docs_occur(string search_query, Dictionary<string, int>[] Dictionarys)
+    static Dictionary<string, int> Query_and_docs_occur(string search_query, Dictionary<string, int>[] Dictionarys)
     {
         string[] search_query_array = search_query.Split();
         Dictionary<string, int> dict = new Dictionary<string, int>(search_query_array.Length);
@@ -275,7 +165,7 @@ public static class Moogle
         }
         return dict;
     }
-    public static List<string> GetNecessaryWords(string query)
+    static List<string> GetNecessaryWords(string query)
     {
         string[] query_array = query.Split();
         List<string> GetNecessaryWords = new List<string>();
@@ -289,21 +179,21 @@ public static class Moogle
         return GetNecessaryWords;
     }
 
-    public static Dictionary<string, int> GetImportance(string query)
+    static Dictionary<string, int> GetImportance(string query)
     {
         string pattern = @"^\*";
         Regex obj = new Regex(pattern);
 
-        Dictionary<string,int> Importance = new Dictionary<string, int>();
+        Dictionary<string, int> Importance = new Dictionary<string, int>();
         string[] query_array = query.Split();
 
-        foreach(string word in query_array)
+        foreach (string word in query_array)
         {
-            if(obj.IsMatch(word))
+            if (obj.IsMatch(word))
             {
                 int asterisc = 0;
                 int index = 0;
-                while(word[index].ToString() == "*")
+                while (word[index].ToString() == "*")
                 {
                     index += 1;
                     asterisc += 1;
@@ -313,65 +203,4 @@ public static class Moogle
         }
         return Importance;
     }
-
-
-    // //Cuenta las ocurrencias de cada palabra de una oracion en un texto y las suma
-    // public static int SentenceCountOc(string text, string sentence)
-    // {
-    //     string[] words = sentence.Split();
-    //     int totalOccur = 0;
-
-    //     foreach (string word in words)
-    //     {
-    //         // TFidf = tf(text, word) * idf(word)
-    //         totalOccur += Moogle.RawCount(text, word);
-    //     }
-
-    //     return totalOccur;
-    // }
-    //Ordena Un Array De SearchItems de mayor a menor segun el Score RESULTS PROCESSING
-
-
 }
-// SearchItem[] items =
-// {
-//     // FilesOccur[0],
-//     // FilesOccur[1],
-//     // FilesOccur[2]
-
-//     new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
-//     new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
-//     new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
-// };
-
-// items = new SearchItem[3];
-
-
-// //Cuenta las ocurrencias de una palabra en un texto
-// public static int RawCount(string text, string subs)
-// {
-//     if (subs == "")
-//         return 0;//No pincha porque el substring es ""
-//     int occurrences = 0;
-//     int start = text.IndexOf(subs);
-
-//     text = text.ToLower();
-//     subs = subs.ToLower();
-
-//     if (start != -1)
-//     {
-//         occurrences++;
-//         occurrences = occurrences + RawCount(text.Substring(start + subs.Length), subs);
-//     }
-//     return occurrences;
-// }
-
-// 0.01905176112949586
-// 0.004127538426740262
-// 0.0005930665979622655
-// 0.0027132793128165906
-// 0.00010239393220392612
-// 0.0004787893682015874
-// 0.001105896176125092
-// 0.0013115821798971503
-// 0.0026231643597943006
--- Testin2/Program.cs
+++ Testin2/Program.cs
@@ -3,8 +3,6 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System;
 
-
-
 Dictionary<string, int>[] GetWordDictionarys(string[] files)
 {
     int FilesLength = files.Length;
@@ -27,10 +25,6 @@ Dictionary<string, int>[] GetWordDictionarys(string[] files)
     return Dictionarys;
 }
 
-
-
-
-
 int[,] GetMatrix(string[] files, Dictionary<string, int>[] Dictionarys, List<string> AllTheWords)
 {
     int[,] Matrix = new int[files.Length, AllTheWords.Count];
@@ -46,15 +40,6 @@ int[,] GetMatrix(string[] files, Dictionary<string, int>[] Dictionarys, List<str
     return Matrix;
 }
 
-void PrintMatrix(int[,] Matrix)
-{
-    for (int x = 0; x < Matrix.GetLength(0); x++)
-    {
-        for (int y = 0; y < Matrix.GetLength(1); y++)
-            System.Console.Write(Matrix[x, y] + "  ");
-        System.Console.WriteLine();
-    }
-}
 
 int[] Sentence2Vec(string sentence, List<string> AllTheWords)
 {
@@ -71,10 +56,7 @@ int[] Sentence2Vec(string sentence, List<string> AllTheWords)
 }
 
 
-string content = @"E:\Prog\moogle\moogle-main\Content";
-string[] files = Directory.GetFiles(content);
-
-
+// MAIN: Gets a tfidf Matrix out of the WordsXFiles Matrix
 double[,] tf_idf(int[,] Matrix)
 {
     double[,] tf_idf = new double[Matrix.GetLength(0), Matrix.GetLength(1)];
@@ -134,54 +116,232 @@ double[,] tf_idf(int[,] Matrix)
     }
 }
 
+// AUXILIARI
 
-// void WriteDict(Dictionary<string, int> Dict)
-// {
-//     foreach (string s in Dict.Keys)
-//     {
-//         System.Console.Write("{" + s + ", " + Dict[s] + "} ");
-//     }
-//     System.Console.WriteLine();
-// }
+void PrintMatrix(int[,] Matrix)
+{
+    for (int x = 0; x < Matrix.GetLength(0); x++)
+    {
+        for (int y = 0; y < Matrix.GetLength(1); y++)
+            System.Console.Write(Matrix[x, y] + "  ");
+        System.Console.WriteLine();
+    }
+}
 
-// foreach (string file in files)
-// {
-//     string[] CurrentFile = File.ReadAllText(file).Split();
-//     foreach (string word in CurrentFile)
-//     {
-//         if (!AllTheWords.Contains(word))
-//             AllTheWords.Add(word);
-//     }
-// }
+// OLD MOOGLE
 
-// @"E:\Prog\moogle\moogle-main\SearchDictionarys.txt"
-// foreach (string word in AllTheWords)
-// {
-//     File.AppendAllText(@"E:\Prog\moogle\moogle-main\SearchDictionarys.txt", "\n" + word);
-// }
 
-// File.WriteAllText(@"E:\Prog\moogle\moogle-main\SearchDictionarys.txt",word);
-// File.AppendAllText(@"E:\Prog\moogle\moogle-main\SearchDictionarys.txt", "word");
+        // Dictionary<string, int> query_and_docs_occur = Query_and_docs_occur(search_query, Dictionarys);
 
-// Dictionary<string, int> Query_and_docs_occur(string search_query, Dictionary<string, int>[] Dictionarys, string[] files)
-// {
-//     string[] search_query_array = search_query.Split();
-//     Dictionary<string, int> dict = new Dictionary<string, int>(search_query_array.Length);
+        // SearchItem[] FilesOccur = new SearchItem[files.Length];
 
-//     foreach (string word in search_query_array)
-//         dict.Add(word, 0);
+        // for (int i = 0; i < files.Length; i++)
+        // {
+        //     // Method: Construir el SearchItem
+        //     var text = Dictionarys[i];
+        //     string CurrentFile = File.ReadAllText(files[i]);
+        //     int FileSize = text.Values.Sum();
+        //     string FileName = Path.GetFileNameWithoutExtension(files[i]);
+        //     string snippet = GetSnip(CurrentFile, search_query);
 
-//     for (int i = 0; i < files.Length; i++)
-//     {
-//         string CurrentFile = files[i];
+        //     double Score = tf_idf(query_and_docs_occur, text,Importance, FileSize, files.Length, search_query) * (1 + 1 / GetCLoseWords(CurrentFile, query, text));
 
-//         foreach (string word in search_query_array)
-//         {
-//             if (word == "" || word == " ")
-//                 continue;
-//             else if (Dictionarys[i].Contains(word))
-//                 dict[word] += 1;
-//         }
-//     }
-//     return dict;
-// }
+        //     FilesOccur[i] = new SearchItem(FileName, snippet, (float)Score);
+        // tf(count Occur,CurrentFile)
+        // idf(files.Length,docsOccur)
+    
+
+    // var items = ResultsPreparations.SortSearchItems(FilesOccur);
+
+    // foreach (SearchItem item in items)
+    //     System.Console.WriteLine(item);
+
+    // timer.Stop();
+    // System.Console.WriteLine("Time taken: " + timer.Elapsed.ToString(@"m\:ss\.fff"));
+    // timer.Restart();
+
+    // timer.Start();
+    // var suggestion = "Suggestion.GetSuggestion(files,search_query)";
+    // // var suggestion = Suggestion.GetSuggestion(files,search_query);
+    // timer.Stop();
+    // System.Console.WriteLine("Time taken in suggestion: " + timer.Elapsed.ToString(@"m\:ss\.fff"));
+
+    // return new SearchResult(items, suggestion);// Suggestion.GetSuggestion(files,search_query)
+
+
+
+
+
+    // public static double tf_idf(Dictionary<string, int> Query_docs_occur, Dictionary<string, int> text,Dictionary<string, int> Importance, int words_in_text, int docs_in_corpus, string query)
+    // {
+    //     double tf_idf = 0;
+    //     var imp = 1;
+    //     List<string> NotAllowedWords = GetNotAllowedWords(query);
+    //     List<string> NecessaryWords = GetNecessaryWords(query);
+
+    //     foreach (string word in NecessaryWords)
+    //     {
+    //         if (!text.Keys.Contains(word))
+    //             return tf_idf;
+    //     }
+    //     foreach (string word in NotAllowedWords)
+    //     {
+    //         if (text.Keys.Contains(word))
+    //             return tf_idf;
+    //     }
+    //     foreach (string word in Query_docs_occur.Keys)
+    //     {
+    //         if(Importance.ContainsKey(word))
+    //             imp += Importance[word];
+    //         System.Console.WriteLine(word);
+    //         System.Console.WriteLine(Moogle.tf(word, text) * Moogle.idf(docs_in_corpus, Query_docs_occur[word])*imp);
+    //         // System.Console.WriteLine(imp);
+    //         tf_idf += Moogle.tf(word, text) * Moogle.idf(docs_in_corpus, Query_docs_occur[word])*imp;
+    //         imp = 1;
+    //     }
+
+    //     return tf_idf;
+    // }
+    // public static double tf(string subs, Dictionary<string, int> Dictionary)
+    // {
+    //     int a = 0;
+    //     if (Dictionary.ContainsKey(subs))
+    //         a = Dictionary[subs];
+    //     return ((double)a / (double)Dictionary.Count);
+    // }
+
+
+    // public static double idf(int docs_in_corpus, int docs_with_occur)
+    // {
+    //     if (docs_with_occur == 0 || docs_in_corpus == docs_with_occur)
+    //         return 0;
+    //     return Math.Log(10, (double)docs_in_corpus / (double)docs_with_occur);
+    // }
+
+    // public static List<string> GetNotAllowedWords(string query)
+    // {
+    //     string[] query_array = query.Split();
+    //     List<string> GetNotAllowedWords = new List<string>();
+    //     if (query == "")
+    //         return GetNotAllowedWords;
+    //     foreach (string word in query_array)
+    //     {
+    //         if (word[0].ToString() == "!")
+    //             GetNotAllowedWords.Add(word.Substring(1));
+    //     }
+    //     return GetNotAllowedWords;
+    // }
+    // static public int GetCLoseWords(string text, string query)
+    // {
+    //     string pattern = @"[ ~ ]||[~]||[ ~]||[~ ]";
+    //     Regex obj = new Regex(pattern);
+
+    //     List<int> distances = new List<int>();
+    //     int distance = 0;
+    //     int ans = 0;
+    //     string[] query_array = query.Split();
+
+    //     for (int i = 0; i < query_array.Length - 2; i++)
+    //     {
+    //         int LeftWordIndex = text.IndexOf(query_array[i]);
+    //         int RightWordIndex = text.IndexOf(query_array[i + 2]);
+
+    //         if (obj.IsMatch(query_array[i + 1]) && LeftWordIndex >= 0 && RightWordIndex >= 0)
+    //         {
+    //             distance = (Math.Abs(LeftWordIndex - RightWordIndex));
+    //             distances.Add(distance);
+    //         }
+    //     }
+    //     foreach (int e in distances)
+    //         ans += e;
+
+    //     return ans;
+    // }
+
+    
+    // This method returns a dict with each word in the query and the amount of files in wich it appears
+    // public static Dictionary<string, int> Query_and_docs_occur(string search_query, string[] files)
+    // {
+    //     string[] search_query_array = search_query.Split();
+    //     Dictionary<string, int> dict = new Dictionary<string, int>(search_query_array.Length);
+
+    //     foreach (string word in search_query_array)
+    //         dict.Add(word, 0);
+
+    //     for (int i = 0; i < files.Length; i++)
+    //     {
+    //         string CurrentFile = File.ReadAllText(files[i]);
+
+    //         foreach (string word in search_query_array)
+    //         {
+    //             if (word == "" || word == " ")
+    //                 continue;
+    //             else if (CurrentFile.Contains(word))
+    //                 dict[word] += 1;
+    //         }
+    //     }
+    //     return dict;
+    // }
+
+
+
+    // //Cuenta las ocurrencias de cada palabra de una oracion en un texto y las suma
+    // public static int SentenceCountOc(string text, string sentence)
+    // {
+    //     string[] words = sentence.Split();
+    //     int totalOccur = 0;
+
+    //     foreach (string word in words)
+    //     {
+    //         // TFidf = tf(text, word) * idf(word)
+    //         totalOccur += Moogle.RawCount(text, word);
+    //     }
+
+    //     return totalOccur;
+    // }
+    //Ordena Un Array De SearchItems de mayor a menor segun el Score RESULTS PROCESSING
+
+
+    // }
+    // SearchItem[] items =
+    // {
+    //     // FilesOccur[0],
+    //     // FilesOccur[1],
+    //     // FilesOccur[2]
+
+    //     new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
+    //     new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
+    //     new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
+    // };
+
+    // items = new SearchItem[3];
+
+
+    // //Cuenta las ocurrencias de una palabra en un texto
+    // public static int RawCount(string text, string subs)
+    // {
+    //     if (subs == "")
+    //         return 0;//No pincha porque el substring es ""
+    //     int occurrences = 0;
+    //     int start = text.IndexOf(subs);
+
+    //     text = text.ToLower();
+    //     subs = subs.ToLower();
+
+    //     if (start != -1)
+    //     {
+    //         occurrences++;
+    //         occurrences = occurrences + RawCount(text.Substring(start + subs.Length), subs);
+    //     }
+    //     return occurrences;
+    // }
+
+    // 0.01905176112949586
+    // 0.004127538426740262
+    // 0.0005930665979622655
+    // 0.0027132793128165906
+    // 0.00010239393220392612
+    // 0.0004787893682015874
+    // 0.001105896176125092
+    // 0.0013115821798971503
+    // 0.0026231643597943006
